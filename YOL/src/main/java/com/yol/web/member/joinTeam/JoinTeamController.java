51d093355b79bb54846d25a9960f6ff167ed4829
diana <diana@192.168.200.119>
2017-11-05 14:15:03 +0900

    앨리아스 주석**

--- YOL/src/main/java/com/yol/web/member/joinTeam/JoinTeamController.java
+++ YOL/src/main/java/com/yol/web/member/joinTeam/JoinTeamController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.yol.web.DTO.VJoinMemberDTO;
 import com.yol.web.DTO.VJoinTeamDTO;
 
 @Controller
@@ -33,5 +34,16 @@ public class JoinTeamController {
 	@RequestMapping(method= {RequestMethod.POST},value="/member/joinTeamAddOk.action")
 	public String boardAddOk() {
 		return "member/joinTeam/boardAddOk";
+	}
+	@RequestMapping(method= {RequestMethod.GET},value="/member/joinTeamView.action")
+	public String boardView(HttpServletRequest req, String reSeq) {
+		
+		VJoinTeamDTO tdto = sv.getDTO(reSeq);
+		req.setAttribute("tdto", tdto);
+		
+		List<VJoinMemberDTO> mlist = sv.joinList(reSeq);
+		req.setAttribute("mlist", mlist);
+		
+		return "member/joinTeam/boardView";
 	}
 }
--- YOL/src/main/java/com/yol/web/member/joinTeam/JoinTeamService.java
+++ YOL/src/main/java/com/yol/web/member/joinTeam/JoinTeamService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.yol.web.DTO.JoinBoardDTO;
+import com.yol.web.DTO.VJoinMemberDTO;
 import com.yol.web.DTO.VJoinTeamDTO;
 
 @Service
@@ -28,6 +29,18 @@ public class JoinTeamService implements IJoinTeamService {
 	public List<VJoinTeamDTO> list() {
 	// 게시판 리스트불러오기
 		return dao.list();
+	}
+
+	@Override
+	public VJoinTeamDTO getDTO(String reSeq) {
+	// 글보기 1개글 불러오기
+		return dao.view(reSeq);
+	}
+
+	@Override
+	public List<VJoinMemberDTO> joinList(String reSeq) {
+	// 신청자 리스트 불러오기
+		return dao.mlist(reSeq);
 	}
 
 }
