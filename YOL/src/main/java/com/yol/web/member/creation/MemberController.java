8ad5688f8176c14d7322f14c08ff91857d79d27d
cryingdev <33308581+cryingdev@users.noreply.github.com>
2017-11-07 20:17:13 +0900

    project creation

--- YOL/src/main/java/com/yol/web/member/creation/MemberController.java
+++ YOL/src/main/java/com/yol/web/member/creation/MemberController.java
@@ -2,6 +2,7 @@ package com.yol.web.member.creation;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MemberController {
 
+	@Autowired
+	private ICreationService ics;
+	
+	
 	@RequestMapping(method = { RequestMethod.GET }, value = "/main.action")
 	public String main(HttpServletRequest req) {
 
@@ -19,8 +24,25 @@ public class MemberController {
 	@RequestMapping(method = { RequestMethod.GET }, value = "/member/home.action")
 	public String home(HttpServletRequest req) {
 
-		return "member.home";
+		
+		//	return "member.home";
+		return "member/home";
 	}
+	
+	
+	// POST로 바꿀 준비
+	
+	@RequestMapping(method = { RequestMethod.GET }, value = "/member/creationstep1.action")
+	public String creationstep1(HttpServletRequest req) {
+
+		return "member/creation/creationstep1";
+	}
+	@RequestMapping(method = { RequestMethod.POST }, value = "/member/creationstep2.action")
+	public String creationstep2(HttpServletRequest req) {
+
+		return "member/creation/creationstep2";
+	}
+		
 	
 	@RequestMapping(method = { RequestMethod.GET }, value = "/member/create.action")
 	public String create(HttpServletRequest req) {
@@ -28,5 +50,11 @@ public class MemberController {
 		return "member/creation/projectadd";
 	}
 	
+	@RequestMapping(method = { RequestMethod.GET }, value = "/member/addproject.action")
+	public String addproject(HttpServletRequest req) {
+		
+		
+		return "member/creation/addproject";
+	}
 }
 
--- YOL/src/main/webapp/WEB-INF/views/member/home.jsp
+++ YOL/src/main/webapp/WEB-INF/views/member/home.jsp
@@ -2,34 +2,32 @@
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles" %>    
 
-<!-- 위에꺼 꼭 추가되어야 함!!! -->
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>멤버들이 보는 화면 구성</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Dialog - Default functionality</title>
+  <link rel="stylesheet" href="/web/resources/css/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  function popmodal(){
+    $( "#step1" ).dialog({
+    	width: 450,height:	520,
+    	modal:true
+    });
+  };
+  </script>
 </head>
 <body>
-<!-- assets -->
-
-<tiles:insertAttribute name="asset"></tiles:insertAttribute>
-
-
-		<!-- header -->
-		<tiles:insertAttribute name="membermenu"></tiles:insertAttribute>
-		
-		
-		<!-- sidemenu -->
-		<tiles:insertAttribute name="membersubmenu"></tiles:insertAttribute>
-		
-		<!-- 주업무 -->
-		
-		<!-- footer -->
-		<div id="memberfooter">
-		
-		</div>
-		
-		
+ 
+ <input type="button" id="popup" value="모달체크" onclick="popmodal();"/>
+	 	 
+	<div id="step1" title="Creation" style="display: none;">
+	  <iframe src="/web/member/creationstep1.action" frameborder="0" style="width:98%; height:98%;" ></iframe>
+	</div>
+	 
+ 
 </body>
-
 </html>
