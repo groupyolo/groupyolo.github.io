844e5a3419fbc88b2d69499bfc7ec88d82aeda96
cryingdev <33308581+cryingdev@users.noreply.github.com>
2017-11-16 17:00:08 +0900

    freeboard

--- YOL/src/main/java/com/yol/web/community/freeboard/FBoardService.java
+++ YOL/src/main/java/com/yol/web/community/freeboard/FBoardService.java
@@ -1,5 +1,6 @@
 package com.yol.web.community.freeboard;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,4 +43,18 @@ public class FBoardService implements IFBoardService{
 		List<VFBoardDTO> list=dao.listshort(dto);
 		return list;
 	}
+	
+	@Override
+	public int getTotalCount() {
+		
+		return dao.getTotalCount();
+	}
+	
+	@Override
+	public List<VFBoardDTO> search(HashMap<String, String> map) {
+	
+		List<VFBoardDTO> list=dao.search(map);
+		return list;
+	}
+	
 }
